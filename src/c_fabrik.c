9489ff77dba9a07b7d8e0d0ee4dba365f59f136a
athallahfaathirnugraha <athallahfaathirnugraha@gmail.com>
2024-08-24 08:18:48 +0700

    Revert "add internalLeftAngle & internalRightAngle"
    This reverts commit e1f6805fcbdc89c73d98cbbc104bc83f8e618820.

--- src/c_fabrik.c
+++ src/c_fabrik.c
@@ -48,30 +48,21 @@ joint_t *tailJoint(limb_t *limb)
     return getJoint(limb, jointLen(limb) - 1);
 }
 
-static void internalGetAngles(
-    joint_t *prev,
-    joint_t *curr,
-    joint_t *next,
-    float *outPrev,
-    float *outNext
-) {
-    *outPrev = atan2f(curr->y - prev->y, prev->x - curr->x);
-    *outNext = atan2f(curr->y - next->y, next->x - curr->x);
-}
-
 void getAngles(limb_t *limb, size_t index, float *outPrev, float *outNext)
 {
     joint_t *joint = getJoint(limb, index);
     joint_t *prevJoint = getJoint(limb, index - 1);
     joint_t *nextJoint = getJoint(limb, index + 1);
 
-    internalGetAngles(prevJoint, joint, nextJoint, outPrev, outNext);
+    *outPrev = atan2f(joint->y - prevJoint->y, prevJoint->x - joint->x);
+    *outNext = atan2f(joint->y - nextJoint->y, nextJoint->x - joint->x);
 }
 
-static float internalLeftAngle(joint_t *prev, joint_t *curr, joint_t *next)
+float leftAngle(limb_t *limb, size_t index)
 {
-    float prevAngle, nextAngle;
-    internalGetAngles(prev, curr, next, &prevAngle, &nextAngle);
+    float prevAngle;
+    float nextAngle;
+    getAngles(limb, index, &prevAngle, &nextAngle);
 
     float res = prevAngle - nextAngle;
     while (res < 0) res += 2 * M_PI;
@@ -79,33 +70,16 @@ static float internalLeftAngle(joint_t *prev, joint_t *curr, joint_t *next)
     return res;
 }
 
-float leftAngle(limb_t *limb, size_t index)
+float rightAngle(limb_t *limb, size_t index)
 {
-    joint_t *prev = getJoint(limb, index - 1);
-    joint_t *curr = getJoint(limb, index);
-    joint_t *next = getJoint(limb, index + 1);
-
-    return internalLeftAngle(prev, curr, next);
-}
-
-static float internalRightAngle(joint_t *prev, joint_t *curr, joint_t *next)
-{
-    float prevAngle, nextAngle;
-    internalGetAngles(prev, curr, next, &prevAngle, &nextAngle);
+    float prevAngle;
+    float nextAngle;
+    getAngles(limb, index, &prevAngle, &nextAngle);
 
     float res = nextAngle - prevAngle;
     while (res < 0) res += 2 * M_PI;
 
     return res;
-}
-
-float rightAngle(limb_t *limb, size_t index)
-{
-    joint_t *prev = getJoint(limb, index - 1);
-    joint_t *curr = getJoint(limb, index);
-    joint_t *next = getJoint(limb, index + 1);
-
-    return internalRightAngle(prev, curr, next);
 }
 
 float rightMidAngle(limb_t *limb, size_t index)
@@ -162,8 +136,6 @@ static void pullJoint(joint_t *joint, joint_t *target, float targetDist)
 
     joint->x += deltaX;
     joint->y += deltaY;
-
-    // adjust for min angle
 }
 
 void pullHead(limb_t *limb, float targetX, float targetY)
